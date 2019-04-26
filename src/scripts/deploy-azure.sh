805b23422cbd09e6cdaefeed8853fb9e66f8b9be
Simon Dale <simon.dale@bjss.com>
2019-04-26 19:34:18 +0100

    Added Azure deployment script and updated presentation

---
+++ src/scripts/deploy-azure.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+: ${CLUSTER_NAME=sfw`date +%Y%m%d%S`}
+: ${RESOURCE_GROUP=$CLUSTER_NAME}
+: ${LOCATION="westeurope"}
+: ${VM_OS="WindowsServer2016Datacenter"}
+: ${VM_SKU="Standard_D4_v3"}
+: ${CLUSTER_SIZE="1"}
+: ${VM_PASSWORD=""}
+
+# create the service fabric cluster
+az sf cluster create \
+    --name $CLUSTER_NAME \
+    --resource-group $RESOURCE_GROUP \
+    --location $LOCATION \
+    --vm-sku $VM_SKU \
+    --vm-os $VM_OS \
+    --vm-password ${VM_PASSWORD} \
+    --certificate-subject-name ${CLUSTER_NAME} \
+    --cluster-size ${CLUSTER_SIZE}
+
+# download the PFX file for the cluster
+: ${SECRET_ID=$(az keyvault secret list --vault-name $CLUSTER_NAME --query "[?contentType==\`application/x-pkcs12\`].id | [0]" | sed -e 's/\"//g')}
+az keyvault secret download --id $SECRET_ID --file $CLUSTER_NAME.pfx
+
+# get the node type
+: ${PIP_ID=$(az resource list --resource-group $RESOURCE_GROUP --resource-type Microsoft.Network/publicIPAddresses --query "[?tags.clusterName==\`$CLUSTER_NAME\`].id|[0]" | sed -e 's/\"//g')}
+: ${IP_ADDRESS=$(az resource show --id $PIP_ID --query "properties.ipAddress" | sed -e 's/\"//g')}
+: ${ENDPOINT=$(az resource show --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME --resource-type Microsoft.ServiceFabric/clusters --query "properties.managementEndpoint" | sed -e 's/\"//g')}
+
+echo Endpoint: $ENDPOINT
+echo IP Address: $IP_ADDRESS
+cat $CLUSTER_NAME.pfx
